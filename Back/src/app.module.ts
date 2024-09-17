a4675547812728170aa65384c0793284a69f0f0c
Mauricio Arce <mauarce34@gmail.com>
2024-09-17 18:05:20 -0300

    ADD/ TypeORM conection

--- Back/src/app.module.ts
+++ Back/src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { OrderDetailController } from './order-detail/order-detail.controller';
+import { OrderDetailService } from './order-detail/order-detail.service';
+import { OrderDetailModule } from './order-detail/order-detail.module';
+import { ProductsModule } from './products/products.module';
+import typeormConfig from './config/typeormConfig';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeormConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('typeorm'),
+    }),
+    OrderDetailModule,
+    ProductsModule,
+  ],
+  controllers: [AppController, OrderDetailController],
+  providers: [AppService, OrderDetailService],
 })
 export class AppModule {}
