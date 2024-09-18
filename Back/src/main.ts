a733f3481f41247ad1620b1c1c158ef3c0e90e7c
caratungu <caratungu@gmail.com>
2024-09-18 17:30:49 -0500

    ADD/ cors, json, swagger configuration

--- Back/src/main.ts
+++ Back/src/main.ts
@@ -1,9 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { env } from './config/envCon';
+import * as cors from 'cors';
+import { json } from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const version = require('../package.json').version;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
+  app.use(json());
+
+  const swaagerConfig = new DocumentBuilder()
+    .setTitle('Pampa')
+    .setDescription('')
+    .setVersion(version)
+    .addBearerAuth()
+    .build()
+  
+  const document = SwaggerModule.createDocument(app, swaagerConfig);
+
+  SwaggerModule.setup('api', app, document, {
+    explorer: true,
+    swaggerOptions: {
+      docExpansion: 'none',
+    }
+  })
+
   await app.listen(env.port);
 }
 bootstrap();
