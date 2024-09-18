c9561d6efa71fb0e30527dc84bf1b34e6cab0198
Mauricio Arce <mauarce34@gmail.com>
2024-09-18 15:01:32 -0300

    ADD/ Order Detail module's imports, controllers and providers

--- Back/src/order-detail/order-detail.module.ts
+++ Back/src/order-detail/order-detail.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { OrderDetailRepository } from './order-detail.repository';
+import { OrderDetailService } from './order-detail.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrderDetails } from './order-detail.entity';
+import { OrderDetailController } from './order-detail.controller';
+import { Products } from 'src/products/product.entity';
+import { ProductsRepository } from 'src/products/product.repository';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([OrderDetails, Products])],
+  controllers: [OrderDetailController],
+  providers: [OrderDetailRepository, OrderDetailService, ProductsRepository],
+})
 export class OrderDetailModule {}
