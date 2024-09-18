c113d4981cc80c4d54c13b75c14ab267d1288d12
Mauricio Arce <mauarce34@gmail.com>
2024-09-18 15:01:39 -0300

    ADD/ Order Detail CRUD

--- Back/src/order-detail/order-detail.service.ts
+++ Back/src/order-detail/order-detail.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { OrderDetailRepository } from './order-detail.repository';
+import { OrderDetailDto } from './order-detail.dto';
+import { ProductsRepository } from 'src/products/product.repository';
 
 @Injectable()
-export class OrderDetailService {}
+export class OrderDetailService {
+  constructor(
+    private orderDetailRepository: OrderDetailRepository,
+    private productRepo: ProductsRepository,
+  ) {}
+
+  async getAll() {
+    const orderDetail = await this.orderDetailRepository.getAll();
+    if (orderDetail.length > 0) return orderDetail;
+    else return 'No order detail found';
+  }
+
+  async getById(id: string) {
+    return await this.orderDetailRepository.getById(id);
+  }
+
+  async getByOrder(id: string) {
+    const orderDetail = await this.orderDetailRepository.getByOrder(id);
+    if (orderDetail.length > 0) return orderDetail;
+    else return 'No order detail found';
+  }
+
+  async create(order: OrderDetailDto) {
+    const product = this.productRepo.getById(order.product);
+    if (!product) throw new BadRequestException('Product not found');
+    return await this.orderDetailRepository.create(order);
+  }
+
+  async update(id: string, order: OrderDetailDto) {
+    return await this.orderDetailRepository.update(id, order);
+  }
+
+  async delete(id: string) {
+    return await this.orderDetailRepository.delete(id);
+  }
+}
