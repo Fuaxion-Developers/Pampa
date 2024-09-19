c9c629922e091e6ff3c3dad94172e095859bb5fc
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-09-19 22:05:38 +0200

    ADD/funcionalidad icono

--- front/src/components/Whatsapp/Whatsapp.tsx
+++ front/src/components/Whatsapp/Whatsapp.tsx
@@ -1,15 +1,22 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Whatsapp = () => {
   return (
     <div className="fixed bottom-8 right-8">
-      <Image
-        src="https://res.cloudinary.com/dkobjvdgn/image/upload/v1726677958/icons8-whatsapp_ndirqi.svg"
-        alt="Whatsapp"
-        width={50}
-        height={50}
-      />
+      <Link
+        href="https://wa.me/5215512345678" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+      >
+        <Image
+          src="https://res.cloudinary.com/dkobjvdgn/image/upload/v1726677958/icons8-whatsapp_ndirqi.svg"
+          alt="Whatsapp"
+          width={50}
+          height={50}
+        />
+      </Link>
     </div>
   );
 };
