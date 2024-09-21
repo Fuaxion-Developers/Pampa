8edabaaea254c927e482e5a431c375149208432d
caratungu <caratungu@gmail.com>
2024-09-20 21:27:46 -0500

    ADD/ getAllUsers, getUserByCuitl, changePass and FIX/ , getUserByEmail

--- Back/src/users/users.repository.ts
+++ Back/src/users/users.repository.ts
@@ -10,26 +10,66 @@ export class UsersRepository {
     @InjectRepository(User) private usersRepository: Repository<User>,
   ) {}
 
+  async getAllUsers({ page, limit }: { page: number, limit: number }): Promise<User[]> {
+    const queryBuilder = this.usersRepository
+      .createQueryBuilder('users')
+      .withDeleted()
+      .select([
+        'users.id',
+        'users.email',
+        'users.deletedDate',
+        'users.role',
+      ])
+      .leftJoinAndSelect('users.info_user', 'info_user')
+      .skip((page - 1) * limit)
+      .take(limit);
+
+    return await queryBuilder.getMany();
+  }
+
   async getUserByEmail(email: string): Promise<User> {
-    const user: User = await this.usersRepository.findOne({
-      where: {
-        email,
-      },
-      select: {
-        id: true,
-        email: true,
-        password: true,
-        resetPasswordExpires: true,
-        resetPasswordToken: true,
-        deletedDate: true,
-      },
-      withDeleted: true,
-    });
-    return user;
+    const userByEmail = this.usersRepository
+      .createQueryBuilder('users')
+      .withDeleted()
+      .where('users.email = :email', { email })
+      .select([
+        'users.id',
+        'users.email',
+        'users.password',
+        'users.deletedDate',
+        'users.role',
+        'users.info_user',
+      ])
+      .leftJoinAndSelect('users.info_user', 'info_user')
+      .getOne();
+
+    return userByEmail;
+  }
+
+  async getUserByCuitl(cuitl: string): Promise<User> {
+    const userByCuitl = this.usersRepository
+      .createQueryBuilder('users')
+      .withDeleted()
+      .select([
+        'users.id',
+        'users.email',
+        'users.deletedDate',
+        'users.role',
+        'users.info_user',
+      ])
+      .leftJoinAndSelect('users.info_user', 'info_user')
+      .where('info_user.cuit_cuil = :cuitl', { cuitl })
+      .getOne();
+
+    return userByCuitl;
   }
 
   async signUp(user: Omit<User, 'id' | 'resetPasswordToken' | 'resetPasswordExpires' | 'deletedDate'>): Promise<User> {
     const userCreated: User = await this.usersRepository.save(user);
     return userCreated;
+  }
+
+  async changePass(userToUpdate: User): Promise<User> {
+    return this.usersRepository.save(userToUpdate);
   }
 }
