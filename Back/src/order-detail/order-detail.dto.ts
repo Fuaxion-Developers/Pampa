ed4b3336d7b02684059532ff43f5d1907f09ccb2
Mauricio Arce <mauarce34@gmail.com>
2024-09-23 14:37:54 -0300

    ADD/ validation in order-detail dto

--- Back/src/order-detail/order-detail.dto.ts
+++ Back/src/order-detail/order-detail.dto.ts
@@ -1,6 +1,15 @@
+import { IsNotEmpty, IsNumber, IsString, IsUUID } from 'class-validator';
 import { v4 as uuidv4 } from 'uuid';
 export class OrderDetailDto {
+  @IsUUID()
+  @IsNotEmpty()
   order: uuidv4;
+
+  @IsUUID()
+  @IsNotEmpty()
   product: string;
+
+  @IsNumber()
+  @IsNotEmpty()
   quantity: number;
 }
