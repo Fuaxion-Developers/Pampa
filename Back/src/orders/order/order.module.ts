f84962b7a5e4023a24de2b6e72cfe615cc05cc88
Mauricio Arce <mauarce34@gmail.com>
2024-09-23 14:38:51 -0300

    ADD/ import in order module

--- Back/src/orders/order/order.module.ts
+++ Back/src/orders/order/order.module.ts
@@ -4,11 +4,12 @@ import { OrderController } from './order.controller';
 import { OrderRepository } from './order.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Orders } from './order.entity';
-import { Products } from 'src/products/product.entity';
-import { ProductsRepository } from 'src/products/product.repository';
+import { InfoUsersService } from 'src/infoUsers/infoUsers.service';
+import { InfoUsersRepository } from 'src/infoUsers/infoUsers.repository';
+import { InfoUsersModule } from 'src/infoUsers/infoUsers.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Orders])],
+  imports: [TypeOrmModule.forFeature([Orders]), InfoUsersModule],
   providers: [OrderService, OrderRepository],
   controllers: [OrderController],
 })
