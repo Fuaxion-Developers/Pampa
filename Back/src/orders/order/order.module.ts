a723e20874d78c3e0737ea822a053121e04a925b
Mauricio Arce <mauarce34@gmail.com>
2024-09-24 15:49:01 -0300

    ADD/ export and import orderDetail module

--- Back/src/orders/order/order.module.ts
+++ Back/src/orders/order/order.module.ts
@@ -7,10 +7,17 @@ import { Orders } from './order.entity';
 import { InfoUsersService } from 'src/infoUsers/infoUsers.service';
 import { InfoUsersRepository } from 'src/infoUsers/infoUsers.repository';
 import { InfoUsersModule } from 'src/infoUsers/infoUsers.module';
+import { OrderDetailController } from 'src/order-detail/order-detail.controller';
+import { OrderDetailModule } from 'src/order-detail/order-detail.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Orders]), InfoUsersModule],
+  imports: [
+    TypeOrmModule.forFeature([Orders]),
+    InfoUsersModule,
+    OrderDetailModule,
+  ],
   providers: [OrderService, OrderRepository],
   controllers: [OrderController],
+  exports: [OrderService],
 })
 export class OrderModule {}
