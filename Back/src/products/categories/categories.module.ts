af2acc704a4230f8a3bb230fe8cbb86bc5c40bf4
Mauricio Arce <mauarce34@gmail.com>
2024-09-24 15:57:40 -0300

    FIX/ file name (products-type to categories)

---
+++ Back/src/products/categories/categories.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { CategoriesController } from './categories.controller';
+import { CategoriesRepository } from './categories.repository';
+import { CategoriesService } from './categories.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Categories } from './categories.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Categories])],
+  controllers: [CategoriesController],
+  providers: [CategoriesService, CategoriesRepository],
+  exports: [CategoriesService],
+})
+export class CategoriesModule {}
