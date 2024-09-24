d29b3cd71f3f374e0b877e22850518e29e30fc02
Mauricio Arce <mauarce34@gmail.com>
2024-09-24 15:51:24 -0300

    ADD/ swagger documentation to productdto and ADD his partialType

--- Back/src/products/product.dto.ts
+++ Back/src/products/product.dto.ts
@@ -1,21 +1,26 @@
 import { IsDecimal, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 
-export class productDto {
+export class productWhitTypeDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ example: 'Calendario Pequeño' })
   name: string;
 
   @IsNotEmpty()
   @IsDecimal({ decimal_digits: '2' })
+  @ApiProperty({ example: 10.5 })
   price: number;
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty({ example: 10 })
   stock: number;
-}
 
-export class productWhitTypeDto extends productDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ example: 'Calendarios' })
   category: string;
 }
+
+export class productWithTypePatchDto extends PartialType(productWhitTypeDto) {}
