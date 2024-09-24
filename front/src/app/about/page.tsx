e52865b7263f380299321b73a4b904b52ed6d4b5
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-09-24 22:02:15 +0200

    pagina de prueba

--- front/src/app/about/page.tsx
+++ front/src/app/about/page.tsx
@@ -1,9 +1,11 @@
+import Prueba from '@/components/prueba'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
       ESTA ES LA VISTA DE ABOUT
+      <Prueba/>
     </div>
   )
 }
