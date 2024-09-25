41bc457e328f50a3db501b733d0cec463428c732
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-09-25 22:23:58 +0200

    ADD/Layout para loguin

---
+++ front/src/app/login/LoginLayout.tsx
@@ -0,0 +1,14 @@
+import "../globals.css";
+
+
+export default function LoginLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+}
