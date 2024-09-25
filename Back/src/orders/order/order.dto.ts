b511ae3337f8f29fcfdfffbb11c45fd7de40aa64
Mauricio Arce <mauarce34@gmail.com>
2024-09-25 13:40:57 -0300

    ADD/ order status to order dto and documentation

--- Back/src/orders/order/order.dto.ts
+++ Back/src/orders/order/order.dto.ts
@@ -2,6 +2,7 @@ import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { v4 as uuid } from 'uuid';
 import {
   IsDateString,
+  IsEmpty,
   IsNotEmpty,
   IsNumberString,
   IsString,
@@ -18,7 +19,22 @@ export class OrderDto {
 
   @IsDateString()
   @IsNotEmpty()
+  @ApiProperty({
+    example: new Date(),
+  })
   date: Date;
+
+  @IsEmpty()
+  order_status: string;
 }
 
 export class OrderDtoPartial extends PartialType(OrderDto) {}
+
+export class OrderWithStatusDto extends OrderDto {
+  @IsUUID()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: uuid(),
+  })
+  order_status: uuid;
+}
