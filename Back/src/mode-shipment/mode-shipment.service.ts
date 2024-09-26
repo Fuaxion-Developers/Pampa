06a2c8b005005dffabbee25a3773043773ca6ad6
Mauricio Arce <mauarce34@gmail.com>
2024-09-26 14:32:46 -0300

    ADD/ CRUD on mode-shipment service

--- Back/src/mode-shipment/mode-shipment.service.ts
+++ Back/src/mode-shipment/mode-shipment.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { ModeShipmentRepository } from './mode-shipment.repository';
+import { v4 as uuid } from 'uuid';
+import { ModeShipmentDto, ModeShipmentParcialDto } from './mode-shipment.dto';
 
 @Injectable()
-export class ModeShipmentService {}
+export class ModeShipmentService {
+  constructor(private modeShipmentRepository: ModeShipmentRepository) {}
+
+  async getAll() {
+    const MS = await this.modeShipmentRepository.getAll();
+    return MS;
+  }
+
+  async getById(id: uuid) {
+    const MS = await this.modeShipmentRepository.getById(id);
+    return MS;
+  }
+
+  async getByName(name: string) {
+    const MS = await this.modeShipmentRepository.getByName(name);
+    return MS;
+  }
+
+  async getByPrice(price: number) {
+    const MS = await this.modeShipmentRepository.getByPrice(price);
+    return MS;
+  }
+
+  async create(modeShipment: ModeShipmentDto) {
+    return await this.modeShipmentRepository.create(modeShipment);
+  }
+
+  async update(id: uuid, modeShipment: ModeShipmentParcialDto) {
+    return await this.modeShipmentRepository.update(id, modeShipment);
+  }
+
+  async delete(id: uuid) {
+    return await this.modeShipmentRepository.delete(id);
+  }
+}
