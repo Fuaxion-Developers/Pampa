05fc5cadcc786cc4118e71a12c09741552b1a570
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-09-30 22:00:21 +0200

    ADD/BAner

---
+++ front/src/components/Productos/BanerCategories/BanerCategories.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const BanerCategories = () => {
+  return (
+    <div className="bg-brownD-100 flex justify-between p-4 text-whiteD-100 my-4">
+      <div className="">
+        <h2 className="text-3xl">PRODUCTOS</h2>
+      </div>
+      <div className='flex'>
+        <Image
+          alt="Imagen"
+          src="https://res.cloudinary.com/dkobjvdgn/image/upload/v1727721484/Rectangle_9605_mp9jx6.png"
+          width={300}
+          height={300}
+        />
+        <Image
+          alt="Imagen"
+          src="https://res.cloudinary.com/dkobjvdgn/image/upload/v1727722911/Rectangle_9601_p4rj7z.png"
+          width={500}
+          height={400}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default BanerCategories;
