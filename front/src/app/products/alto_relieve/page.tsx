f2eac39cddf85deca4f7a5d26c75f6316947bada
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-09-30 23:20:44 +0200

    page

--- front/src/app/products/alto_relieve/page.tsx
+++ front/src/app/products/alto_relieve/page.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      
+    <div className='h-32 w-[15%] overflow-y-auto p-4 space-y-4 m-8'>
+      <p className='bg-red-600'>Linea Roja</p>
+      <p className='bg-green-700'>Linea Verde</p>
+      <p className='bg-indigo-700'>Linea Violeta</p>
+      <p className='bg-yellow-400'>Linea Amarilla</p>
+      <p className='bg-blue-700'>Linea Azul</p>
+      <p className='bg-orange-600'>Linea Naranja</p>
     </div>
   )
 }
