ec0cc355b76335a70154f1729d733432f87423a8
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-10-01 19:47:22 +0200

    FIX/modificacion en page

--- front/src/app/products/page.tsx
+++ front/src/app/products/page.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
-
+'use client'
+import React, { useEffect } from 'react';
 const page = () => {
-  return <div></div>;
+  // Aseguramos que la página siempre haga scroll al inicio al cargar.
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div>
+    </div>
+  );
 };
 
 export default page;
