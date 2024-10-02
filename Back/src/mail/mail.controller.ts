a8cc31efaec8b0a3a347491462af420dc612c639
caratungu <caratungu@gmail.com>
2024-10-01 21:58:37 -0500

    ADD/ MailControler

---
+++ Back/src/mail/mail.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, HttpStatus, Post, Res } from "@nestjs/common";
+import { Response } from "express";
+import { MailService } from "./mail.service";
+import { SendEmailDto } from "./dtos/sendEmail.dto";
+
+@Controller('mail')
+export class MailController {
+    constructor(
+        private mailService: MailService,
+    ){}
+
+    @Post('send-mail')
+    async sendEmail(@Body() body: SendEmailDto, @Res() res: Response){
+        try {
+            const response = await this.mailService.sendEmail(body);
+            res.status(HttpStatus.OK).send(response)
+        } catch (error) {
+            throw error;
+        }
+    }
+}
