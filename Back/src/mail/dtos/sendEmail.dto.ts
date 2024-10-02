b905fd2de8ae9388c530dfc17a0fe70bc6f1da5d
caratungu <caratungu@gmail.com>
2024-10-01 21:59:40 -0500

    ADD/ SendEmailDto

---
+++ Back/src/mail/dtos/sendEmail.dto.ts
@@ -0,0 +1,24 @@
+import { IsEnum, IsNotEmpty, IsObject, IsOptional, IsString } from "class-validator";
+import { Templates } from "../enums/templates.enum";
+
+export class SendEmailDto {
+    @IsString()
+    @IsNotEmpty()
+    from: string;
+
+    @IsString()
+    @IsNotEmpty()
+    subjectEmail: string;
+
+    @IsString()
+    @IsNotEmpty()
+    sendTo: string;
+
+    @IsEnum(Templates)
+    @IsNotEmpty()
+    template: string;
+
+    @IsObject()
+    @IsOptional()
+    params: any;
+}
