e9e1f08b84be33e0f567ad53e400af682ed0619e
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-10-09 21:54:36 +0200

    FIX

--- front/src/components/Login/Login.tsx
+++ front/src/components/Login/Login.tsx
@@ -8,10 +8,8 @@ import { useRouter } from 'next/navigation';
 import Swal from 'sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 import PasswordInput from '../Password/PasswordImput';
-// import { decodeJWT } from '../../helpers/decodeJwt';
-// import { useUser } from '@auth0/nextjs-auth0/client';
 import Link from 'next/link';
-import RestorePasswordModal from '../RestorePassword/RestorePassword';
+import RestorePasswordModal from '../PasswordRestore/RestorePassword/RestorePassword';
 
 const Login = () => {
   const router = useRouter();
@@ -19,22 +17,19 @@ const Login = () => {
     email: '',
     password: '',
   });
-  // const { user, error, isLoading } = useUser();
-
 
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-
-
   const [errorUser, setErrorUser] = useState<LoginErrorProps>({
     email: '',
     password: '',
   });
 
   useEffect(() => {
-    const userSession = localStorage.getItem('userSession');
-    if (userSession) {
-      router.push('/');
+    if (typeof window !== 'undefined') {
+      const userSession = localStorage.getItem('userSession');
+      if (userSession) {
+        router.push('/');
+      }
     }
   }, [router]);
 
@@ -54,35 +49,45 @@ const Login = () => {
       try {
         const response = await login(dataUser);
         const { token, userInfoToReturn } = response;
+
+        // Guardamos el token y la información del usuario en el localStorage
         localStorage.setItem(
           'userSession',
           JSON.stringify({ token: token, userInfoToReturn })
         );
+
+        // Redirigimos inmediatamente después de guardar la sesión
+        router.push('/');
+
+        // Luego mostramos el mensaje de éxito con SweetAlert
         await Swal.fire({
           title: '¡Excelente!',
-          text: `${userInfoToReturn.first_name}, has iniciado sesión correctamente.`,
+          text: `${userInfoToReturn.info_user.first_name}, has iniciado sesión correctamente.`,
           icon: 'success',
           confirmButtonText: 'Aceptar',
-          background: '#1D1D1D', // Cambia este valor al color de fondo que prefieras
+          background: '#1D1D1D',
           customClass: {
             confirmButton:
               'hover:scale-110 bg-greenD-500 text-black font-bold py-2 px-4 rounded',
-            title: 'text-greenD-500', // Cambia el color del texto del título
-            popup: 'text-white', // Cambia el color del texto del contenido
+            title: 'text-greenD-500',
+            popup: 'text-white',
           },
         });
       } catch (error: any) {
+        const errorMessage =
+          error.response?.data?.message ||
+          'Hubo un problema al iniciar sesión. Por favor, intente de nuevo.';
         Swal.fire({
           title: 'Error',
-          text: 'Hubo un problema al iniciar sesión. Por favor, intente de nuevo.',
+          text: errorMessage,
           icon: 'error',
           confirmButtonText: 'Aceptar',
-          background: '#1D1D1D', // Cambia este valor al color de fondo que prefieras
+          background: '#1D1D1D',
           customClass: {
             confirmButton:
               'hover:scale-110 bg-greenD-500 text-black font-bold py-2 px-4 rounded',
-            title: 'text-red-500', // Cambia el color del texto del título
-            popup: 'text-white', // Cambia el color del texto del contenido
+            title: 'text-red-500',
+            popup: 'text-white',
           },
         });
       }
@@ -91,17 +96,16 @@ const Login = () => {
 
   const inputClass =
     'h-[28px] p-5 items-center gap-[10px] self-stretch border border-gray-300 w-full';
-  const labelClass = ' text-[15px] font-medium leading-normal';
+  const labelClass = 'text-[15px] font-medium leading-normal';
 
   return (
     <div className="flex justify-center items-center h-[100vh]">
       <div className="flex w-full h-full max-h-[1024px]">
         <a
           href="/"
-          className="flex flex-row justify-center items-center absolute top-4 left-4  gap-2"
+          className="flex flex-row justify-center items-center absolute top-4 left-4 gap-2"
         >
           <Image
-            className="" // Ajusta el tamaño según tus necesidades
             src="https://res.cloudinary.com/dkobjvdgn/image/upload/v1727295377/arrow_back_24dp_927363_FILL0_wght400_GRAD0_opsz24_qepr5l.svg"
             alt="Volver"
             width={30}
@@ -109,16 +113,15 @@ const Login = () => {
           />
         </a>
 
-        <div className="w-[50%] flex flex-col items-center justify-center bg-whiteD-200  p-12">
+        <div className="w-[50%] flex flex-col items-center justify-center bg-whiteD-200 p-12">
           <Image
-            className=" flex justify-center items-center" // Ajusta el tamaño según tus necesidades
             src="https://res.cloudinary.com/dkobjvdgn/image/upload/v1726763511/Pampa_chico_1_bwdcha.svg"
             alt="Volver"
             width={300}
             height={300}
           />
           <div className="flex flex-col items-start justify-start mb-8">
-            <h2 className=" text-[20px] font-semibold leading-normal mb-4">
+            <h2 className="text-[20px] font-semibold leading-normal mb-4">
               INICIA SESIÓN EN PAMPA
             </h2>
           </div>
@@ -165,7 +168,7 @@ const Login = () => {
                 isOpen={isModalOpen}
                 onClose={() => setIsModalOpen(false)}
               />
-              <div className="w-full  mt-4 flex justify-center">
+              <div className="w-full mt-4 flex justify-center">
                 <button
                   type="submit"
                   className="flex w-full h-[38px] px-[25px] py-[11px] justify-center items-center gap-[10px] rounded-[5px] bg-brownD-100"
@@ -178,8 +181,8 @@ const Login = () => {
             </form>
           </div>
 
-          <div className="w-full max-w-[80%]  mt-8 flex flex-row items-center justify-center gap-4">
-            <p className="">No tienes cuenta?</p>
+          <div className="w-full max-w-[80%] mt-8 flex flex-row items-center justify-center gap-4">
+            <p>No tienes cuenta?</p>
             <a href="/register" className="text-brownD-100">
               Regístrate
             </a>
@@ -190,7 +193,7 @@ const Login = () => {
             className="hidden md:block"
             src="https://res.cloudinary.com/dkobjvdgn/image/upload/v1727297153/pexels-cottonbro-3838325_1_1_t2s6tu.png"
             alt="Login Image"
-            layout="fill"
+            fill
             priority
           />
         </div>
