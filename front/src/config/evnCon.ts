43166aea8b8a2e5849a4466c88e152f772f0bca1
Mauricio Arce <mauarce34@gmail.com>
2024-10-21 11:32:59 -0300

    ADD/ mercado pago keys

--- front/src/config/evnCon.ts
+++ front/src/config/evnCon.ts
@@ -1,8 +1,12 @@
-import 'dotenv/config';
-import { config as dotenvConfig } from 'dotenv';
+import "dotenv/config";
+import { config as dotenvConfig } from "dotenv";
 
-dotenvConfig({ path: ['./.env.development', './.env'] });
+dotenvConfig({ path: ["./.env.development", "./.env"] });
 
 export const env = {
   backUrl: process.env.NEXT_PUBLIC_BACK_URL,
+  MercadoPago: {
+    MP_ACCESS_TOKEN: process.env.NEXT_PUBLIC_MP_ACCESS_TOKEN || "",
+    MP_PUBLIC_KEY: process.env.NEXT_PUBLIC_MP_PUBLIC_KEY || "",
+  },
 };
