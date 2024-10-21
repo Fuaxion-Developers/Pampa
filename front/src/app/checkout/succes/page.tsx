a4c138cc05b9d8791b5f9dcb7ded2f4eddcafdcc
Mauricio Arce <mauarce34@gmail.com>
2024-10-21 11:31:14 -0300

    ADD/ succes page

---
+++ front/src/app/checkout/succes/page.tsx
@@ -0,0 +1,10 @@
+import { Success } from "@/components/MercadoPago/succes";
+import React from "react";
+
+export default function Succes() {
+  return (
+    <div>
+      <Success />
+    </div>
+  );
+}
