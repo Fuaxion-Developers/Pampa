5be0877ce30948ef02a8f80122c4b0b4911f4117
Mauricio Arce <mauarce34@gmail.com>
2024-10-21 11:28:17 -0300

    ADD/ export users service

--- Back/src/users/users.module.ts
+++ Back/src/users/users.module.ts
@@ -1,15 +1,16 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { User } from "./users.entity";
-import { UsersController } from "./users.controller";
-import { UsersService } from "./users.service";
-import { UsersRepository } from "./users.repository";
-import { InfoUsersModule } from "src/infoUsers/infoUsers.module";
-import { MailModule } from "src/mail/mail.module";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users.entity';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { UsersRepository } from './users.repository';
+import { InfoUsersModule } from 'src/infoUsers/infoUsers.module';
+import { MailModule } from 'src/mail/mail.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User]), InfoUsersModule, MailModule],
-    controllers: [UsersController],
-    providers: [UsersService, UsersRepository]
+  imports: [TypeOrmModule.forFeature([User]), InfoUsersModule, MailModule],
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository],
+  exports: [UsersService],
 })
 export class UsersModule {}
