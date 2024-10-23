37857316bce5ff057ca4f4feed1e0a949cc589d9
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-10-23 21:57:51 +0200

    FIX

--- front/src/app/admin/AdminLayout.tsx
+++ front/src/app/admin/AdminLayout.tsx
@@ -1,5 +1,6 @@
 import Sidebar from '@/components/Admin/sidebar/Sidebar';
 import '../globals.css';
+import NavbarAdmin from '@/components/Admin/NavbarAdmin/NavbarAdmin';
 
 export default function AdminLayout({
   children,
@@ -7,8 +8,10 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex bg-whiteD-200">
+    <div className="flex  bg-whiteD-200 ">
+      {' '}
       <Sidebar />
+      <NavbarAdmin />
       {children}
     </div>
   );
