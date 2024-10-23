0fa4adbdebbbf0d9c711989e35b9942ac5ae4691
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-10-23 21:58:09 +0200

    FIX

---
+++ front/src/app/admin/products/page.tsx
@@ -0,0 +1,12 @@
+import ProductList from '@/components/Admin/Products/ProductsList';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div className="w-[70%] ml-[25%] mt-[10%]">
+      <ProductList></ProductList>
+    </div>
+  );
+};
+
+export default page;
