cc3ba30553aec26a39c4b3c01745fa7dabfdba6a
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2024-10-23 21:57:55 +0200

    FIX

--- front/src/app/admin/page.tsx
+++ front/src/app/admin/page.tsx
@@ -1,24 +1,12 @@
-import Admin from '@/components/Admin/Admin';
-import Categorias from '@/components/Admin/categories/Categories';
-import Clientes from '@/components/Admin/Clientes/clientes';
-import Navbar from '@/components/Admin/Nav/Navbar';
-import Productos from '@/components/Admin/Products/Products';
-import Sidebar from '@/components/Admin/sidebar/Sidebar';
-import Ventas from '@/components/Admin/Ventas/Ventas';
+import Dashboard from '@/components/Admin/Dashboard';
 import React from 'react';
 
 const page = () => {
   return (
-    <div className=''>
-<Navbar/>
-  <div className='grid-cols-2 grid m-8 gap-8' >
-<Clientes/>
-<Categorias/>
-<Productos/>
-<Ventas/>
+    <div className="w-[70%] ml-[25%] mt-[10%]">
+     <Dashboard/>
     </div>
-    </div>
-    )
+  );
 };
 
 export default page;
