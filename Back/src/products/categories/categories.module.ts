9a037e61d2dacd34998c6bf170332dbf8d0cafc8
Mauricio Arce <mauarce34@gmail.com>
2024-11-04 15:20:32 -0300

    ADD/ import to categories module

--- Back/src/products/categories/categories.module.ts
+++ Back/src/products/categories/categories.module.ts
@@ -4,9 +4,10 @@ import { CategoriesRepository } from './categories.repository';
 import { CategoriesService } from './categories.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Categories } from './categories.entity';
+import { FilesModule } from 'src/files/files.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Categories])],
+  imports: [TypeOrmModule.forFeature([Categories]), FilesModule],
   controllers: [CategoriesController],
   providers: [CategoriesService, CategoriesRepository],
   exports: [CategoriesService],
