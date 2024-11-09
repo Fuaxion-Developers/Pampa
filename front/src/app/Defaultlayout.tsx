ea84862c2839bb7683c9b6340b3f71624c02a939
RafaelGarcia <mgrafaelgarcia.2805@gmail.com>
2024-11-09 18:14:38 -0500

    delete relative and z index

--- front/src/app/Defaultlayout.tsx
+++ front/src/app/Defaultlayout.tsx
@@ -3,21 +3,27 @@ import Navbar from "../components/Navbar/Navbar";
 
 import React from "react";
 import Whatsapp from "@/components/Whatsapp/Whatsapp";
+import Footer from "@/components/Footer/Footer";
 export default function DefaultLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative z-0 ">
+    <div className="">
       <Navbar />
-      <div className="flex-grow flex items-center justify-center p-4 relative z-0">
+      {/* <div className="flex-grow flex items-center justify-center p-4 relative z-0">
         <div className="w-full relative max-w-7xl h-[84vh] bg-black/30 backdrop-blur-md rounded-3xl flex flex-col overflow-hidden shadow-lg scrollbar-thin scrollbar-thumb-yellow-400 scrollbar-track-transparent z-0">
           <main className="flex-grow overflow-y-auto scrollbar-thin scrollbar-thumb-yellow-400 scrollbar-track-transparent scrollbar-thumb-rounded-xl relative z-0">
             {children}
           </main>
         </div>
+      </div> */}
+      <div >
+        {children}
+        
       </div>
+      <Footer/>
     </div>
   );
 }
